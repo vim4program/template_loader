b437d537c4993d8636cd1cf3f8ecb3d1de80e53c
vim4program <shanghai1980@gmail.com>
2012-09-16 08:44:00 +0800

    first version

---
+++ templates/tpl.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file <T_FILENAME>
+ * @author <T_AUTHOR>(<T_AUTHOR_EMAIL>)
+ * @date <T_CREATE_DATE>
+ * @brief
+ *
+ **/
+
+#include "<T_HEADER_FILE>"
+
+<T_CURSOR>
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100 */
---
+++ templates/tpl.h
@@ -0,0 +1,16 @@
+/**
+ * @file <T_FILENAME>
+ * @author <T_AUTHOR>(<T_AUTHOR_EMAIL>)
+ * @date <T_CREATE_DATE>
+ * @brief
+ *
+ **/
+
+#ifdef <T_HEADER_H>
+#define <T_HEADER_H>
+
+<T_CURSOR>
+
+#endif // <T_HEADER_H>
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100 */
